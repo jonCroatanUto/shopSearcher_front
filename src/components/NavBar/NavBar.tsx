acdf2d85db0104f7f44fc734b8a13601c2ed023b
JonCroatan <43881219+jonCroatanUto@users.noreply.github.com>
2021-12-06 18:06:01 +0100

    searcher of navBar in process...

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -99,7 +99,7 @@ function NavBar() {
               <input
                 className="form-control me-2"
                 type="search"
-                placeholder="Search"
+                placeholder="Searcher in process..."
                 aria-label="Search"
               />
               <button className="btn btn-outline-success" type="submit">
